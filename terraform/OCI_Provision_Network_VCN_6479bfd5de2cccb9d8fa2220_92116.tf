46ad65e517bff7ae8086e9cca0fb4a9f230d6ca3
root <root@qa-app-server>
2023-06-02 12:37:32 +0000

    OCI_Provision_Network_VCN - terraform template updated1

---
+++ terraform/OCI_Provision_Network_VCN_6479bfd5de2cccb9d8fa2220_92116.tf
@@ -0,0 +1,8 @@
+module "OCI_Provision_Network_VCN_6479bfd5de2cccb9d8fa2220_92116" {
+  source           = ""
+  oci_region       = "us-phoenix-1"
+  vcn_name         = "module_instance_flex_network"
+  freeform_tags    = { "type" : "test_tag" }
+  compartment_ocid = "ocid1.compartment.oc1..aaaaaaaaqkl2ofrdamcvb7jsokdrnol62npos3p2xofx6hbr3ks4b6wief7a"
+  vcn_cidr_block   = "10.0.0.0/24"
+}
