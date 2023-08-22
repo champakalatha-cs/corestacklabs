44b5dd177d3773a96d033dc15d1b2e105de6aae0
root <root@qa-app-server>
2023-08-22 09:41:38 +0000

    instance-provisioninghj - terraform template updated

--- terraform/instance-provisioninghj_64d1e2f91d985e3190b0fe1a_93356.tf
+++ terraform/instance-provisioninghj_64d1e2f91d985e3190b0fe1a_93356.tf
@@ -1,4 +1,4 @@
 module "instance-provisioninghj_64d1e2f91d985e3190b0fe1a_93356" {
   source              = "git::https://github.com/seshanb/powershell_terraform.git"
-  resource_group_name = "QA_INFRA_RG"
+  resource_group_name = "QA_INFRA_RG_Testing"
 }
