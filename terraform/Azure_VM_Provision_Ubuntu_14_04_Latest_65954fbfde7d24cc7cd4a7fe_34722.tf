cb7e995de04df310b5d8eda5496b050fc982a99b
root <root@cs-sbx-app-01>
2024-01-09 09:33:46 +0000

    OCI_Provision_Network_VCN - terraform template updated

--- terraform/Azure_VM_Provision_Ubuntu_14_04_Latest_65954fbfde7d24cc7cd4a7fe_34722.tf
+++ terraform/Azure_VM_Provision_Ubuntu_14_04_Latest_65954fbfde7d24cc7cd4a7fe_34722.tf
@@ -1,13 +1,8 @@
 module "Azure_VM_Provision_Ubuntu_14_04_Latest_65954fbfde7d24cc7cd4a7fe_34722" {
-  source                        = ""
-  azure_Resource_Group          = "QA_INFRA_RG"
-  location                      = "East US"
-  azure_Virtual_Network_Name    = "testVirtualNetworkats"
-  address_Space                 = ["10.0.0.0/16"]
-  azure_Subnet_Name             = "Subnet1sytr"
-  address_Prefix                = ["10.0.2.0/24"]
-  azure_Network_Interface_Name  = "testnetworkinterface1saytr"
-  IP_Configuration_Name         = "testconfiguration-private-ip-ahdg"
-  private_IP_Address_Allocation = "Dynamic"
-  prefix                        = "testazure"
+  source           = ""
+  oci_region       = "us-phoenix-1"
+  vcn_name         = "module_instance_flex_network"
+  freeform_tags    = { "type" : "test_tag" }
+  compartment_ocid = "ocid1.compartment.oc1..aaaaaaaaqkl2ofrdamcvb7jsokdrnol62npos3p2xofx6hbr3ks4b6wief7a"
+  vcn_cidr_block   = "10.0.0.0/24"
 }
